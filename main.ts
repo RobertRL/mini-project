59ec788b383b9de695ebfefd88a9a80c81cdc51b
RobertRL <71644110+RobertRL@users.noreply.github.com>
2020-09-21 14:47:34 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,24 @@
 input.onButtonPressed(Button.A, function () {
-    led.setBrightness(led.brightness() + 10)
+    led.setBrightness(led.brightness() + gain)
+    led.plotBrightness(x, y, 255)
 })
+input.onButtonPressed(Button.B, function () {
+	
+})
+let gain = 0
+let x = 0
+let y = 0
 led.enable(true)
 led.setBrightness(0)
+y = 0
+x = 0
+gain = 10
+led.plotBrightness(x, y, led.brightness())
 basic.forever(function () {
-    led.plotBrightness(0, 0, led.brightness())
-    if (led.brightness() <= 255) {
-    	
+    if (led.brightness() >= 255) {
+        led.plotBrightness(x, y, 255)
+        x += 1
+        led.setBrightness(0)
+        led.plotBrightness(x, y, led.brightness())
     }
 })
