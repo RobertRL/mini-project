056412727ab25dfc463434009a4336b8fba6b318
RobertRL <71644110+RobertRL@users.noreply.github.com>
2020-09-22 16:35:41 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,9 +5,10 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     if (Coin >= 1) {
         if (y >= 1) {
-            gain += 10
-            y = y - 1
+            gain += gain * 2
+            y = 0
             x = 0
+            basic.clearScreen()
         }
     }
 })
@@ -21,21 +22,37 @@ led.enable(true)
 led.setBrightness(0)
 y = 0
 x = 0
-gain = 100
+gain = 10
 Coin = 0
 led.plotBrightness(x, y, brightness)
 basic.forever(function () {
     brightness = led.brightness()
     Led = x
-    if (brightness >= 250) {
+    if (brightness >= 255) {
         if (Led >= 4) {
             y += 1
             x = 0
         }
-        led.plotBrightness(x, y, 250)
+        led.plotBrightness(x, y, 136)
         x += 1
         led.setBrightness(0)
         led.plotBrightness(x, y, brightness)
         Coin += 1
+    }
+    if (x == 4 && y == 4) {
+        while (x == 4 == (y == 4)) {
+            basic.showIcon(IconNames.SmallDiamond)
+            basic.showIcon(IconNames.SmallSquare)
+            basic.showIcon(IconNames.Diamond)
+            basic.showIcon(IconNames.Square)
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                `)
+            basic.clearScreen()
+        }
     }
 })
